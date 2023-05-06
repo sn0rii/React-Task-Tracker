809d65088446058469b5a2c9c732a19840384775
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-06 13:16:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -25,6 +25,13 @@ function App() {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 1000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete Task
 
   const deleteTask = (id) => {
@@ -43,7 +50,7 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
